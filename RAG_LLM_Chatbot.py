014901cdbbcd96b2a5f14bd32316352521d2df08
Nonny-123 <nonnyokonji@gmail.com>
2025-07-30 22:56:40 +0100

    llm commit

--- RAG_LLM_Chatbot.py
+++ RAG_LLM_Chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
+from langchain.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
